f85686bdbf80f3d72c42c495c2d545663e2f1cae
Jacobo <tadeo.jacobo6@gmail.com>
2024-07-07 23:47:19 -0400

    interface

--- React-ts/src/App.tsx
+++ React-ts/src/App.tsx
@@ -2,26 +2,14 @@ import { useState } from 'react'
 
 function App() {
 
-  const [subs, setSubs] = useState([
-    {
-      nick: 'Pablo',
-      subMonths: 3,
-      avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
-      description: 'Pablo is a moderator in the chat'
-    },
-    {
-      nick: 'Juan',
-      subMonths: 5,
-      avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
-      description: 'Juan is a subscriber'
-    },
-    {
-      nick: 'Pedro', 
-      subMonths: 1,
-      avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
-      description: 'Pedro is a subscriber'
-    }
-  ])
+  interface Sub {
+    nick: string
+    subMonths: number
+    avatar: string
+    description?: string
+  }
+
+  const [subs, setSubs] = useState<Array<Sub>>([])
 
 
   return (
