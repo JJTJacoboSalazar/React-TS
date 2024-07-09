cbbd1b7fba631dcd3755fa8d63624f58eb15e1a7
Jacobo <tadeo.jacobo6@gmail.com>
2024-07-09 16:12:54 -0400

    handle change form

--- React-ts/src/components/Form.tsx
+++ React-ts/src/components/Form.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 const Form = () => {
+    const [inputValues, setInputValues] = useState({
+        nick: '',
+        subMonths: 0,
+        avatar: '',
+        description: ''
+    })
 
     const handleSubmit = () => {
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValues({
+            ...inputValues,
+            [e.target.placeholder]: e.target.value
+        })
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Nick" />
-                <input type="number" placeholder="Sub months" />
-                <input type="text" placeholder="Avatar" />
-                <textarea placeholder="Description" />
+                <input onChange={handleChange} value={inputValues.nick} type="text" placeholder="Nick" />
+                <input onChange={handleChange} value={inputValues.subMonths} type="number" placeholder="Sub months" />
+                <input onChange={handleChange} value={inputValues.avatar} type="text" placeholder="Avatar" />
+                <input onChange={handleChange} value={inputValues.description} type="text" placeholder="description" />
                 <button type="submit">Add</button>
             </form>
         </div>
